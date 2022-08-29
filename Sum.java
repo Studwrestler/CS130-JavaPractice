d445e2acd6e732f5cacecdd6c03cc22f39f59035
Studwrestler <112411998+Studwrestler@users.noreply.github.com>
2022-08-29 11:51:55 -0400

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum
+{
+public static void main(String[]args)
+{
+int number1=3;
+int number2=5;
+int sum=number1+number2;
+int product=number1*number2;
+int difference=number1-number2;
+System.out.println(sum);
+System.out.println(product);
+System.out.println(difference);
+}
+}
